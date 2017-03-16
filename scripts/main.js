41ce2d1cfb23953b5ef4da51e2498a18e6f474a5
Jeremy Bost <bostjs1@gcc.edu>
2017-03-15 21:19:26 -0400

    Some more comments and error checking

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
 var selectedCourses = {};
 
+
 // name parameter is in the form "ACCT 202 A"
 function addCourse(name) {
     // TODO: add checks for errors
-    if(!selectedCourses[name] && allCourses[name])
+    if (!selectedCourses[name] && allCourses[name]) {
         selectedCourses[name] = allCourses[name];
+    }
+    else {
+        // error
+        console.log("Error adding '" + name + "' to selected courses");
+    }
 }
+
 
 // name parameter is in the form "ACCT 202 A"
 function removeCourse(name) {
-    // This shouldn't cause errors if course doesn't exist, but haven't tested that
+    // This shouldn't cause errors if course doesn't exist
     delete selectedCourses[name];
 }
 
+
+// clear all selected courses
 function clearCourses() {
     selectedCourses = {};
 }
 
+
+// returns an array of strings containing the course codes of currently selected courses
 function getSelectedCourseCodes() {
     // TODO: actually test this
     courseCodes = [];
