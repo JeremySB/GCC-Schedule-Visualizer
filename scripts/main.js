f019893e18b599d1443c6b1af9f66b859afddacc
Christian Talbot <christiantalbot@Christians-MacBook-Pro.local>
2017-04-24 21:25:06 -0400

    Cafe times added

--- scripts/main.js
+++ scripts/main.js
@@ -131,6 +131,210 @@ var tutorial = new Tour({
     ]
 });
 
+//Cafeteria meal times
+var mapTimes = [
+    {
+      title : 'MondayBreakfast'
+      start : 'T07:15:00'
+      end : 'T10:00:00'
+    },
+    {
+      title : 'MondayLunch'
+      start : 'T11:45:00'
+      end : 'T13:45:00'
+    },
+    {
+      title : 'MondayDinner'
+      start : 'T16:30:00'
+      end : 'T19:15:00'
+    },
+    {
+      title : 'TuesdayBreakfast'
+      start : 'T07:15:00'
+      end : 'T10:00:00'
+    },
+    {
+      title : 'TuesdayLunch'
+      start : 'T11:45:00'
+      end : 'T13:45:00'
+    },
+    {
+      title : 'TuesdayDinner'
+      start : 'T16:30:00'
+      end : 'T19:15:00'
+    },
+    {
+      title : 'WednesdayBreakfast'
+      start : 'T07:15:00'
+      end : 'T10:00:00'
+    },
+    {
+      title : 'WednesdayLunch'
+      start : 'T11:45:00'
+      end : 'T13:45:00'
+    },
+    {
+      title : 'WednesdayDinner'
+      start : 'T16:30:00'
+      end : 'T19:15:00'
+    },
+    {
+      title : 'ThursdayBreakfast'
+      start : 'T07:15:00'
+      end : 'T10:00:00'
+    },
+    {
+      title : 'ThursdayLunch'
+      start : 'T11:45:00'
+      end : 'T13:45:00'
+    },
+    {
+      title : 'ThursdayDinner'
+      start : 'T16:30:00'
+      end : 'T19:15:00'
+    },
+    {
+      title : 'FridayBreakfast'
+      start : 'T07:15:00'
+      end : 'T10:00:00'
+    },
+    {
+      title : 'FridayLunch'
+      start : 'T11:45:00'
+      end : 'T13:45:00'
+    },
+];
+
+var hicksTimes = [
+    {
+      title : 'MondayLunch'
+      start : 'T10:50:00'
+      end : 'T13:00:00'
+    },
+    {
+      title : 'MondayDinner'
+      start : 'T16:00:00'
+      end : 'T18:30:00'
+    },
+    {
+      title : 'TuesdayLunch'
+      start : 'T10:50:00'
+      end : 'T13:00:00'
+    },
+    {
+      title : 'TuesdayDinner'
+      start : 'T16:00:00'
+      end : 'T18:30:00'
+    },
+    {
+      title : 'WednesdayLunch'
+      start : 'T10:50:00'
+      end : 'T13:00:00'
+    },
+    {
+      title : 'WednesdayDinner'
+      start : 'T16:00:00'
+      end : 'T18:30:00'
+    },
+    {
+      title : 'ThursdayLunch'
+      start : 'T10:50:00'
+      end : 'T13:00:00'
+    },
+    {
+      title : 'ThursdayDinner'
+      start : 'T16:00:00'
+      end : 'T18:30:00'
+    },
+    {
+      title : 'FridayLunch'
+      start : 'T10:50:00'
+      end : 'T13:00:00'
+    },
+    {
+      title : 'FridayDinner'
+      start : 'T16:00:00'
+      end : 'T19:15:00'
+    }
+];
+
+var sacTimes = [
+    {
+      title : 'MondayBreakfast'
+      start : 'T07:00:00'
+      end : 'T09:00:00'
+    },
+    {
+      title : 'MondayLunch'
+      start : 'T12:35:00'
+      end : 'T15:45:00'
+    },
+    {
+      title : 'MondayDinner'
+      start : 'T16:00:00'
+      end : 'T19:30:00'
+    },
+    {
+      title : 'TuesdayBreakfast'
+      start : 'T07:00:00'
+      end : 'T09:00:00'
+    },
+    {
+      title : 'TuesdayLunch'
+      start : 'T12:35:00'
+      end : 'T15:45:00'
+    },
+    {
+      title : 'TuesdayDinner'
+      start : 'T16:00:00'
+      end : 'T19:30:00'
+    },
+    {
+      title : 'WednesdayBreakfast'
+      start : 'T07:00:00'
+      end : 'T09:00:00'
+    },
+    {
+      title : 'WednesdayLunch'
+      start : 'T12:35:00'
+      end : 'T15:45:00'
+    },
+    {
+      title : 'WednesdayDinner'
+      start : 'T16:00:00'
+      end : 'T19:30:00'
+    },
+    {
+      title : 'ThursdayBreakfast'
+      start : 'T07:00:00'
+      end : 'T09:00:00'
+    },
+    {
+      title : 'ThursdayLunch'
+      start : 'T12:35:00'
+      end : 'T15:45:00'
+    },
+    {
+      title : 'ThursdayDinner'
+      start : 'T16:00:00'
+      end : 'T19:30:00'
+    },
+    {
+      title : 'FridayBreakfast'
+      start : 'T07:00:00'
+      end : 'T09:00:00'
+    },
+    {
+      title : 'FridayLunch'
+      start : 'T12:35:00'
+      end : 'T15:45:00'
+    },
+    {
+      title : 'FridayDinner'
+      start : 'T16:00:00'
+      end : 'T17:30:00'
+    }
+];
 
 // quick utility function to pad numbers with 0's on the left
 function pad(num, size) {
@@ -408,8 +612,8 @@ function searchCourses(query) {
         displaySearchResults();
         return;
     }
-	
-	// 
+
+	//
 	if (departmentNames[query]) {
 		query = departmentNames[query];
 	}
