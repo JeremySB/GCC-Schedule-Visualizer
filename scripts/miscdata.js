49d5bc810c2a4936422a5394d43059aec137f206
Daniel Toney <toneydr1@gcc.edu>
2017-04-24 22:48:35 -0400

    Fixed the times and added background redering to the meal times.

--- scripts/miscdata.js
+++ scripts/miscdata.js
@@ -1,204 +1,240 @@
 //Cafeteria meal times
-var mapTimes = [
-    {
+var mapTimes = [{
         title: 'MondayBreakfast',
-      start : 'T07:15:00',
-      end : 'T10:00:00',
+        start: '2016-08-01T07:15:00',
+        end: '2016-08-01T10:00:00',
+        rendering: 'background'
     },
     {
         title: 'MondayLunch',
-      start : 'T11:45:00',
-      end : 'T13:45:00',
+        start: '2016-08-01T11:45:00',
+        end: '2016-08-01T13:45:00',
+        rendering: 'background'
     },
     {
         title: 'MondayDinner',
-      start : 'T16:30:00',
-      end : 'T19:15:00',
+        start: '2016-08-01T16:30:00',
+        end: '2016-08-01T19:15:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayBreakfast',
-      start : 'T07:15:00',
-      end : 'T10:00:00',
+        start: '2016-08-02T07:15:00',
+        end: '2016-08-02T10:00:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayLunch',
-      start : 'T11:45:00',
-      end : 'T13:45:00',
+        start: '2016-08-02T11:45:00',
+        end: '2016-08-02T13:45:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayDinner',
-      start : 'T16:30:00',
-      end : 'T19:15:00',
+        start: '2016-08-02T16:30:00',
+        end: '2016-08-02T19:15:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayBreakfast',
-      start : 'T07:15:00',
-      end : 'T10:00:00',
+        start: '2016-08-03T07:15:00',
+        end: '2016-08-03T10:00:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayLunch',
-      start : 'T11:45:00',
-      end : 'T13:45:00',
+        start: '2016-08-03T11:45:00',
+        end: '2016-08-03T13:45:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayDinner',
-      start : 'T16:30:00',
-      end : 'T19:15:00',
+        start: '2016-08-03T16:30:00',
+        end: '2016-08-03T19:15:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayBreakfast',
-      start : 'T07:15:00',
-      end : 'T10:00:00',
+        start: '2016-08-04T07:15:00',
+        end: '2016-08-04T10:00:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayLunch',
-      start : 'T11:45:00',
-      end : 'T13:45:00',
+        start: '2016-08-04T11:45:00',
+        end: '2016-08-04T13:45:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayDinner',
-      start : 'T16:30:00',
-      end : 'T19:15:00',
+        start: '2016-08-04T16:30:00',
+        end: '2016-08-04T19:15:00',
+        rendering: 'background'
     },
     {
         title: 'FridayBreakfast',
-      start : 'T07:15:00',
-      end : 'T10:00:00',
+        start: '2016-08-04T07:15:00',
+        end: '2016-08-04T10:00:00',
+        rendering: 'background'
     },
     {
         title: 'FridayLunch',
-      start : 'T11:45:00',
-      end : 'T13:45:00',
+        start: '2016-08-05T11:45:00',
+        end: '2016-08-05T13:45:00',
+        rendering: 'background'
     },
 ];
 
-var hicksTimes = [
-    {
+var hicksTimes = [{
         title: 'MondayLunch',
-      start : 'T10:50:00',
-      end : 'T13:00:00',
+        start: '2016-08-01T10:50:00',
+        end: '2016-08-01T13:00:00',
+        rendering: 'background'
     },
     {
         title: 'MondayDinner',
-      start : 'T16:00:00',
-      end : 'T18:30:00',
+        start: '2016-08-01T16:00:00',
+        end: '2016-08-01T18:30:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayLunch',
-      start : 'T10:50:00',
-      end : 'T13:00:00',
+        start: '2016-08-02T10:50:00',
+        end: '2016-08-02T13:00:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayDinner',
-      start : 'T16:00:00',
-      end : 'T18:30:00',
+        start: '2016-08-02T16:00:00',
+        end: '2016-08-02T18:30:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayLunch',
-      start : 'T10:50:00',
-      end : 'T13:00:00',
+        start: '2016-08-03T10:50:00',
+        end: '2016-08-03T13:00:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayDinner',
-      start : 'T16:00:00',
-      end : 'T18:30:00',
+        start: '2016-08-03T16:00:00',
+        end: '2016-08-03T18:30:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayLunch',
-      start : 'T10:50:00',
-      end : 'T13:00:00',
+        start: '2016-08-04T10:50:00',
+        end: '2016-08-04T13:00:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayDinner',
-      start : 'T16:00:00',
-      end : 'T18:30:00',
+        start: '2016-08-04T16:00:00',
+        end: '2016-08-04T18:30:00',
+        rendering: 'background'
     },
     {
         title: 'FridayLunch',
-      start : 'T10:50:00',
-      end : 'T13:00:00',
+        start: '2016-08-05T10:50:00',
+        end: '2016-08-05T13:00:00',
+        rendering: 'background'
     },
     {
         title: 'FridayDinner',
-      start : 'T16:00:00',
-      end : 'T19:15:00',
+        start: '2016-08-05T16:00:00',
+        end: '2016-08-05T19:15:00',
+        rendering: 'background'
     }
 ];
 
-var sacTimes = [
-    {
+var sacTimes = [{
         title: 'MondayBreakfast',
-      start : 'T07:00:00',
-      end : 'T09:00:00',
+        start: '2016-08-01T07:00:00',
+        end: '2016-08-01T09:00:00',
+        rendering: 'background'
     },
     {
         title: 'MondayLunch',
-      start : 'T12:35:00',
-      end : 'T15:45:00',
+        start: '2016-08-01T12:35:00',
+        end: '2016-08-01T15:45:00',
+        rendering: 'background'
     },
     {
         title: 'MondayDinner',
-      start : 'T16:00:00',
-      end : 'T19:30:00',
+        start: '2016-08-01T16:00:00',
+        end: '2016-08-01T19:30:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayBreakfast',
-      start : 'T07:00:00',
-      end : 'T09:00:00',
+        start: '2016-08-02T07:00:00',
+        end: '2016-08-02T09:00:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayLunch',
-      start : 'T12:35:00',
-      end : 'T15:45:00',
+        start: '2016-08-02T12:35:00',
+        end: '2016-08-02T15:45:00',
+        rendering: 'background'
     },
     {
         title: 'TuesdayDinner',
-      start : 'T16:00:00',
-      end : 'T19:30:00',
+        start: '2016-08-02T16:00:00',
+        end: '2016-08-02T19:30:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayBreakfast',
-      start : 'T07:00:00',
-      end : 'T09:00:00',
+        start: '2016-08-03T07:00:00',
+        end: '2016-08-03T09:00:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayLunch',
-      start : 'T12:35:00',
-      end : 'T15:45:00',
+        start: '2016-08-03T12:35:00',
+        end: '2016-08-03T15:45:00',
+        rendering: 'background'
     },
     {
         title: 'WednesdayDinner',
-      start : 'T16:00:00',
-      end : 'T19:30:00',
+        start: '2016-08-03T16:00:00',
+        end: '2016-08-03T19:30:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayBreakfast',
-      start : 'T07:00:00',
-      end : 'T09:00:00',
+        start: '2016-08-04T07:00:00',
+        end: '2016-08-04T09:00:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayLunch',
-      start : 'T12:35:00',
-      end : 'T15:45:00',
+        start: '2016-08-04T12:35:00',
+        end: '2016-08-04T15:45:00',
+        rendering: 'background'
     },
     {
         title: 'ThursdayDinner',
-      start : 'T16:00:00',
-      end : 'T19:30:00',
+        start: '2016-08-04T16:00:00',
+        end: '2016-08-04T19:30:00',
+        rendering: 'background'
     },
     {
         title: 'FridayBreakfast',
-      start : 'T07:00:00',
-      end : 'T09:00:00',
+        start: '2016-08-05T07:00:00',
+        end: '2016-08-05T09:00:00',
+        rendering: 'background'
     },
     {
         title: 'FridayLunch',
-      start : 'T12:35:00',
-      end : 'T15:45:00',
+        start: '2016-08-05T12:35:00',
+        end: '2016-08-05T15:45:00',
+        rendering: 'background'
     },
     {
         title: 'FridayDinner',
-      start : 'T16:00:00',
-      end : 'T17:30:00',
+        start: '2016-08-05T16:00:00',
+        end: '2016-08-05T17:30:00',
+        rendering: 'background'
     }
 ];
