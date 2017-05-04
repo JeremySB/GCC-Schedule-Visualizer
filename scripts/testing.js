1e2b1b8425c60ac62bab60713c63f8ab9e3bf746
Daniel Toney <toneydr1@gcc.edu>
2017-05-04 18:34:43 -0400

    Testing code

---
+++ scripts/testing.js
@@ -0,0 +1,28 @@
+testingCourses = Object.freeze({
+
+});
+
+
+$(function () {
+    // SSet up stuff
+    // Test days of week
+    // Test course numbers
+    // Test Department
+    // Test time of day
+});
+
+function FilterDays(){
+
+}
+
+function FilterTimes(){
+
+}
+
+function FilterDepartment(){
+
+}
+
+function FilterNumber(){
+
+}
